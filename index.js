ec120c064a016c9a548264a966db6da94e84dcfe
William3210 <42059856+William3210@users.noreply.github.com>
2018-08-04 12:55:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const botconfig = require("./botconfig.json");
-const client = new Discord.Client();
 const Discord = require("discord.js");
+const client = new Discord.Client();
 const fs = require("fs");
-const bot = new Discord.Client({disableEveryone: true});
-bot.commands = new Discord.Collection();
+client.commands = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) => {
 
@@ -18,41 +16,41 @@ fs.readdir("./commands/", (err, files) => {
   jsfile.forEach((f, i) =>{
     let props = require(`./commands/${f}`);
     console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
+    client.commands.set(props.help.name, props);
   });
 });
 
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} is ready to kick ass!`);
+client.on("ready", async () => {
+  console.log(`${client.user.username} is ready to kick ass!`);
 
-  bot.user.setActivity("You", {type: "WATCHING"});
+  client.user.setActivity("You", {type: "WATCHING"});
 });
 
-bot.on("guildMemberAdd", async member => {
+client.on("guildMemberAdd", async member => {
   console.log(`${member.id} joined the server.`);
 
   let welcomechannel = member.guild.channels.find(`name`, "welcome");
   welcomechannel.send(`LOOK OUT EVERYONE! ${member} has just joined the party! :tada: :hugging:`);
 });
 
-bot.on("guildMemberRemove", async member => {
+client.on("guildMemberRemove", async member => {
   console.log(`${member.id} left the server.`);
 
   let welcomechannel = member.guild.channels.find(`name`, "welcome");
   welcomechannel.send(`GOOD RIDDANCE! ${member} Just Bailed On The Server :pensive:`);
 });
 
-bot.on("message", async message => {
-  if(message.author.bot) return;
+client.on("message", async message => {
+  if(message.author.client) return;
   if(message.channel.type === "dm") return;
 
-  let prefix = botconfig.prefix;
+  let prefix = clientconfig.prefix;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(bot,message,args);
+  let commandfile = client.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(client,message,args);
 });
 
 client.login(process.env.BOT_TOKEN);
